7dff3851d7a7bdaa206916b31b8652eb3f37975e
GerdModzel <196028553+GerdModzel@users.noreply.github.com>
2025-01-26 11:01:03 +0100

    parse() uses a loop to remove the first char from text, then call walkThroughNfa()
    - This has to be done because each char could be the start of a matching string, which cannot be found any other way
    - ParseResult is properly constructed.

--- libRegEx/parse.cpp
+++ libRegEx/parse.cpp
@@ -1,13 +1,26 @@
 ﻿#include "parse.h"
 
+#include "NfaState.h"
+
 #include <iostream>
 #include <span>
 
 namespace regex {
 
   std::vector<ParseResult> parse(std::string_view text, std::string_view expr) {
-    std::cout << expr << std::endl;
-    return {};
+    if (text.empty() || expr.empty())
+      return {};
+
+    const auto [nfa, stateManager] = buildNfaList(expr);
+    std::vector<ParseResult> results;
+    
+    for (int i = 0; i < text.size(); ++i) {
+      std::string_view subText = text.substr(i, text.size());
+      const unsigned size = walkThroughNfa(nfa.startState, subText);
+      if (size > 0)
+        results.emplace_back(i, size);
+    }
+    return results;
   }
 
 }
