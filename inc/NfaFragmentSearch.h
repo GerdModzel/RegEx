f90416e66aaee664b9b2a557ba8578f1cbe7925f
GerdModzel <196028553+GerdModzel@users.noreply.github.com>
2025-07-21 16:40:00 +0200

    added comments to NfaFragmentSearch

--- inc/NfaFragmentSearch.h
+++ inc/NfaFragmentSearch.h
@@ -9,6 +9,16 @@
 
   class NfaFragment;
 
+  /**
+   * \brief Executes a search for matches of the NFA fragment in the given text.
+   * 
+   * This function processes the input text character by character, updating the current state of the NFA
+   * and collecting matches as they are found.
+   * 
+   * \param text The text to search for matches.
+   * \param fragment The NFA fragment to use for matching.
+   * \return A vector of ParseResult objects representing the matches found in the text.
+   */
   std::vector<ParseResult> executeSearch(std::string_view text, NfaFragment* fragment);
 
 }
--- libRegEx/NfaFragmentSearch.cpp
+++ libRegEx/NfaFragmentSearch.cpp
@@ -6,45 +6,59 @@
 #include <vector>
 #include <cassert>
 
+
 namespace regex {
 
-  void addStateToList(std::vector<NfaState*>& list, NfaState* state, const int counter)
-  {
-    assert(state != nullptr);
-    if (state->lastList == counter)
-      return; // already added to the list
-    if (state->type == NfaState::Type::split) {
-      for (const auto& nextState : state->nextStates)
-        addStateToList(list, nextState, counter);
-      return;
-    }
-    state->lastList = counter;
-    list.push_back(state);
-  }
+  namespace {
 
-  void step(const std::vector<NfaState*>& currentStateList, std::vector<NfaState*>& nextStateList, char ch, const int counter) {
-    for (const auto currentState : currentStateList) {
-      if (currentState->type == NfaState::Type::ch && currentState->ch.value_or(ch) == ch) { // matches character and wildcard (wildcard has no value)
-        assert(currentState->nextStates.size() == 1);
-        addStateToList(nextStateList, currentState->nextStates[0], counter);
+    /* Adds the state to the list if it has not been added yet.
+    */
+    void addStateToList(std::vector<NfaState*>& list, NfaState* state, const int counter)
+    {
+      assert(state != nullptr);
+      if (state->lastList == counter)
+        return; // already added to the list
+      if (state->type == NfaState::Type::split) {
+        for (const auto& nextState : state->nextStates)
+          addStateToList(list, nextState, counter);
+        return;
+      }
+      state->lastList = counter;
+      list.push_back(state);
+    }
+
+    /* Processes the current state list and checks if the character matches any of the NfaStates.
+    */
+    void step(const std::vector<NfaState*>& currentStateList, std::vector<NfaState*>& nextStateList, char ch, const int counter) {
+      for (const auto currentState : currentStateList) {
+        if (currentState->type == NfaState::Type::ch && currentState->ch.value_or(ch) == ch) { //  wildcard always matches (wildcard has no value)
+          assert(currentState->nextStates.size() == 1);
+          addStateToList(nextStateList, currentState->nextStates[0], counter);
+        }
       }
     }
-  }
 
-  void extractMatches(const std::vector<NfaState*>& stateList, std::vector<ParseResult>& matchList, const int counter) {
-    for (const auto& state : stateList) {
-      if (state->type == NfaState::Type::match)
-        matchList.emplace_back(static_cast<size_t>(counter), 1u);
+    void extractMatches(const std::vector<NfaState*>& stateList, std::vector<ParseResult>& matchList, const int counter) {
+      for (const auto& state : stateList) {
+        if (state->type == NfaState::Type::match)
+          matchList.emplace_back(static_cast<size_t>(counter), 1u);
+      }
     }
-  }
 
+  } // anonymous namespace
+
+  /* When a character is processed, a check is made whether it matches any of the NfaStates in currentStateList.
+  *  If it does, the state's next states are added to nextStateList. If any of the next states is a match state,
+  *  a match was found and a ParseResult is created. For the next loop, nextStateList becomes the new currentStateList,
+  *  and the new nextStateList is cleared except for the start state. The start state is always on the list.
+  */
   std::vector<ParseResult> executeSearch(std::string_view text, NfaFragment* fragment) {
     std::vector<ParseResult> resultList;
     std::vector<NfaState*> cStateList, nStateList;
     std::vector<NfaState*>& currentStateList = cStateList;
     std::vector<NfaState*>& nextStateList = nStateList;
     int characterCounter = 0;
-    
+
     for (const auto ch : text) {
       currentStateList.swap(nextStateList);
       nextStateList.clear();
@@ -56,6 +70,4 @@ namespace regex {
     return resultList;
   }
 
-
-
-}
+} // namespace regex
